23047d51d2a3d7cfef5b620e677db3af6151542f
AreumHi <rladkfma7546@gmail.com>
2025-03-27 23:28:11 +0900

    feat: 포인트 충전/사용 시 이용 내역 저장 기능 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -13,8 +13,7 @@ import static io.hhplus.tdd.common.Constants.MAX_POINT;
 @Service
 public class PointService {
 
-    private final UserPointTable userPointTable;    // 생성자 주입으로 받아서 사용 ( = @RequiredArgsConstructor)
-
+    private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
     public PointService(UserPointTable userPointTable, PointHistoryTable pointHistoryTable) {
@@ -36,23 +35,22 @@ public class PointService {
      * - 포인트 최대 한도를 초과해서는 안된다.
      */
     public UserPoint chargePoint(long userId, long chargeAmount) {
-        // 금액 유효성 체크 (1원 이상인지)
+
         if (chargeAmount <= 0) {
             throw new IllegalArgumentException("충전 금액은 1원 이상이어야 합니다.");
         }
 
-        // 현재 포인트 조회
         UserPoint currentPoint = userPointTable.selectById(userId);
 
         // 충전 후 업데이트 되어야 할 포인트
         long updatePoint = currentPoint.point() + chargeAmount;
 
-        // 최대 한도초과 여부 체크
         if (MAX_POINT < updatePoint) {
             throw new IllegalArgumentException(String.format("최대 포인트 한도(%d)를 초과할 수 없습니다.", MAX_POINT));
         }
 
-        // 포인트 반영
+        pointHistoryTable.insert(userId, chargeAmount, TransactionType.CHARGE, System.currentTimeMillis());
+
         return userPointTable.insertOrUpdate(userId, updatePoint);
     }
 
@@ -62,23 +60,22 @@ public class PointService {
      * - 사용 금액은 1원 이상이어야 한다.
      */
     public UserPoint usePoint(long userId, long useRequestAmount) {
-        // 금액 유효성 체크 (1원 이상인지)
+
         if (useRequestAmount <= 0) {
             throw new IllegalArgumentException("사용 금액은 1원 이상이어야 합니다.");
         }
 
-        // 현재 포인트 조회
         UserPoint currentPoint = userPointTable.selectById(userId);
 
         // 사용 후 업데이트 되어야 할 포인트
         long updatePoint = currentPoint.point() - useRequestAmount;
 
-        // 포인트 잔액 부족한지 여부 체크
         if (updatePoint < 0) {
             throw new IllegalArgumentException(String.format("포인트가 부족합니다. 현재 포인트: %d, 사용 요청 금액: %d", currentPoint.point(), useRequestAmount));
         }
 
-        // 포인트 반영
+        pointHistoryTable.insert(userId, useRequestAmount, TransactionType.USE, System.currentTimeMillis());
+
         return userPointTable.insertOrUpdate(userId, updatePoint);
     }
 
